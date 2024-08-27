e31c75b5d1888b65e6cc924cb05ea35e6aa8a565
shubham <shubzade@amazon.com>
2024-08-27 17:49:58 +0530

    inserting new attende

--- Task3-Event-Management-System/Manage Track Attendees & Handle Events- Insert new attendee.sql
+++ Task3-Event-Management-System/Manage Track Attendees & Handle Events- Insert new attendee.sql
@@ -1,2 +1,4 @@
 INSERT INTO Attendees (Attendee_id,Attendee_Name, Attendee_Phone, Attendee_Email, Attendee_City) VALUES
 (5,'Emily Davis', '567-890-1234', 'emily.davis@example.com', 'Los Angeles');
+
+SELECT * FROM Attendees;
